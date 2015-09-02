4fb66a1dc25837aa598c4021a5699f56b77c4ce9
Gleb Arshinov <gleb-oss@barsook.com>
2015-09-02 19:06:40 -0400

    Rename s/my-magit/magit-global/

--- lisp/magit-global.el
+++ lisp/magit-global.el
@@ -2,17 +2,17 @@
 (global-set-key [(control x) (g) (g)] 'magit-status)
 (global-set-key [(control x) (g) (c)] 'magit-commit-popup)
 (global-set-key [(control x) (g) ($)] 'magit-process)
-(global-set-key [(control x) (g) (l)] 'my-magit-log)
-(global-set-key [(control x) (g) (=)] 'my-magit-diff)
-(global-set-key [(control x) (g) (s)] 'my-magit-stage)
-(global-set-key [(control x) (g) (o)] 'my-magit-opened)
+(global-set-key [(control x) (g) (l)] 'magit-global-log)
+(global-set-key [(control x) (g) (=)] 'magit-global-diff)
+(global-set-key [(control x) (g) (s)] 'magit-global-stage)
+(global-set-key [(control x) (g) (o)] 'magit-global-opened)
 
 
-(defun my-magit-log (&optional args files)
-  (interactive (my-magit-read-args-and-files
+(defun magit-global-log (&optional args files)
+  (interactive (magit-global-read-args-and-files
                 "git log: "
                 magit-log-arguments
-                (-if-let (f (my-magit-context-single-filename))
+                (-if-let (f (magit-global-context-single-filename))
                     (list (expand-file-name f)) nil)
                 nil))
   ;; TODO: something somewhere is messing with current directory, so
@@ -21,11 +21,11 @@
   (magit-log-all args files))
 
 
-(defun my-magit-diff (args files)
-  (interactive (my-magit-read-args-and-files
+(defun magit-global-diff (args files)
+  (interactive (magit-global-read-args-and-files
                 "git diff: "
                 magit-diff-arguments
-                (-if-let (f (my-magit-context-single-filename))
+                (-if-let (f (magit-global-context-single-filename))
                     (list (expand-file-name f)) nil)
                 nil))
   ;; TODO: something somewhere is messing with current directory, so
@@ -34,14 +34,14 @@
   (magit-mode-setup magit-diff-buffer-name-format
                     magit-diff-switch-buffer-function
                     #'magit-status-mode
-                    #'my-magit-diff-refresh-buffer args files)
+                    #'magit-global-diff-refresh-buffer args files)
   ;; TODO: this is a hack, this funtionality logically belongs in
   ;; magit-mode-setup.  Shouldn't invoke switch-buffer-function under
   ;; some conditions.
   (if (zerop (buffer-size))
       (progn (quit-window)
              (message "No changes unstaged or staged"))))
-(defun my-magit-diff-refresh-buffer (args files)
+(defun magit-global-diff-refresh-buffer (args files)
   ;; note that magit-diff-section-arguments must be included for -/+
   ;; to work in magit-status-mode
   (magit-insert-section (status)
@@ -60,40 +60,40 @@
     (magit-section-show-level-4-all)))
 
 
-(defun my-magit-stage ()
+(defun magit-global-stage ()
   (interactive)
   (magit-stage-item buffer-file-name)
   (message "Staged"))
 
 
 ;; equivalent to p4-opened
-(defun my-magit-opened ()
+(defun magit-global-opened ()
   (interactive)
   (let* ((dir default-directory)
          (buf (get-buffer-create "*magit-opened*")))
     (magit-mode-setup buf nil
                       #'magit-status-mode 
-                      #'my-magit-refresh-opened-buffer)
+                      #'magit-global-refresh-opened-buffer)
     (with-current-buffer buf
       (goto-char (point-min))
       (forward-line))
     ))
-(defun my-magit-refresh-opened-buffer ()
+(defun magit-global-refresh-opened-buffer ()
   (magit-insert-unstaged-changes))
 
 
-(defun my-magit-context-single-filename ()
+(defun magit-global-context-single-filename ()
   ;; next 2 lines from magit-log-buffer-file
   (or (buffer-file-name (buffer-base-buffer))
       magit-buffer-file-name
       default-directory))
 
 
-(defun my-magit-read-args-and-files (prompt &optional args
+(defun magit-global-read-args-and-files (prompt &optional args
                                             files-no-prefix
                                             files-with-prefix)
   (if current-prefix-arg
-      (my-magit-s-to-args-and-files
+      (magit-global-s-to-args-and-files
        ;; TODO: what we want here ideally is a full-blown completion
        ;; on git options, their arguments, and files.  Like bash
        ;; completion.  Or like p4-read-arg-string completion for p4
@@ -113,67 +113,67 @@
        ;;
        ;;   (completing-read "git log: "
        ;;   'completion-function-to-be-written nil nil "initial input"
-       ;;   'my-magit-history-variable)
+       ;;   'magit-global-history-variable)
        ;;
        ;; So, for now let's use p4-read-arg-string if available, and
        ;; fallback to read-shell-command otherwise.
        (if (functionp 'p4-read-arg-string)
            (p4-read-arg-string prompt
-                               (my-magit-args-and-files-to-s
+                               (magit-global-args-and-files-to-s
                                 args files-with-prefix))
-         (read-shell-command prompt (my-magit-args-and-files-to-s
+         (read-shell-command prompt (magit-global-args-and-files-to-s
                                 args files-with-prefix))))
     (list args files-no-prefix)))
 
 
-(defun my-magit-s-to-args-and-files (str)
+(defun magit-global-s-to-args-and-files (str)
   (let* ((tokens (split-string-and-unquote str))
          (args (--take-while (not (equal it "--")) tokens))
          (files (cdr (--drop-while (not (equal it "--")) tokens))))
     (list args files)))
 ;; tests
-(my-magit-s-to-args-and-files "--all --graph -- ./myfile.txt ./foo/file.txt")
-(my-magit-s-to-args-and-files "--all --graph -- ./myfile.txt")
-(my-magit-s-to-args-and-files "--all --graph --")
-(my-magit-s-to-args-and-files "--all --graph")
-(my-magit-s-to-args-and-files "")
-(my-magit-s-to-args-and-files "--")
-(my-magit-s-to-args-and-files "-- ./myfile.txt")
+(magit-global-s-to-args-and-files "--all --graph -- ./myfile.txt ./foo/file.txt")
+(magit-global-s-to-args-and-files "--all --graph -- ./myfile.txt")
+(magit-global-s-to-args-and-files "--all --graph --")
+(magit-global-s-to-args-and-files "--all --graph")
+(magit-global-s-to-args-and-files "")
+(magit-global-s-to-args-and-files "--")
+(magit-global-s-to-args-and-files "-- ./myfile.txt")
 ;; quotes and escaping
 ;; basic quoting to deal with spaces
-(my-magit-s-to-args-and-files "--all -S\"find me\"")
+(magit-global-s-to-args-and-files "--all -S\"find me\"")
 ;; this is how you represent a " character, needs both quoting and
 ;; escaping
-(my-magit-s-to-args-and-files "\"foo\\\"bar\"")
+(magit-global-s-to-args-and-files "\"foo\\\"bar\"")
 ;; double quotes can be escaped, but only within double quotes
-;; (my-magit-s-to-args-and-files "foo\\\"bar")  ; error
+;; (magit-global-s-to-args-and-files "foo\\\"bar")  ; error
 ;; single quotes are not supported/special
-(my-magit-s-to-args-and-files "'single quote' \"'foo\"")
+(magit-global-s-to-args-and-files "'single quote' \"'foo\"")
 ;; space cannot be escaped, only quotes can be escaped
-(my-magit-s-to-args-and-files "escaped\\ space")
+(magit-global-s-to-args-and-files "escaped\\ space")
 ;; unbalanced quotes are bad
 ;; this results in error "End of file during parsing
-;; (my-magit-s-to-args-and-files "unbalanced\"quote")
+;; (magit-global-s-to-args-and-files "unbalanced\"quote")
 ;; escape characters by themselves are not treated specially
-(my-magit-s-to-args-and-files "\\ \\")
+(magit-global-s-to-args-and-files "\\ \\")
 
 
-(defun my-magit-args-and-files-to-s (args files)
+(defun magit-global-args-and-files-to-s (args files)
   ;; last empty string to get trailing space for filename completion
   ;; to work at point right away
   (combine-and-quote-strings (append args '("--") files '(""))))
 ;; tests
-(my-magit-args-and-files-to-s '("--all" "--graph")
+(magit-global-args-and-files-to-s '("--all" "--graph")
                               '("./myfile.txt" "./foo/file.txt"))
-(my-magit-args-and-files-to-s '("--all" "--graph") '("./myfile.txt"))
-(my-magit-args-and-files-to-s '("--all" "--graph") nil)
-(my-magit-args-and-files-to-s nil nil)
-(my-magit-args-and-files-to-s nil '("./myfile.txt" "./foo/file.txt"))
+(magit-global-args-and-files-to-s '("--all" "--graph") '("./myfile.txt"))
+(magit-global-args-and-files-to-s '("--all" "--graph") nil)
+(magit-global-args-and-files-to-s nil nil)
+(magit-global-args-and-files-to-s nil '("./myfile.txt" "./foo/file.txt"))
 ;; things needing quoting
-(my-magit-args-and-files-to-s '("-S" "with space" "'single'" "\"double\"") nil)
-(my-magit-args-and-files-to-s nil '("file with space" "\\" "'" "\""))
-(my-magit-args-and-files-to-s nil '("\""))
-(my-magit-args-and-files-to-s nil '("\\"))
+(magit-global-args-and-files-to-s '("-S" "with space" "'single'" "\"double\"") nil)
+(magit-global-args-and-files-to-s nil '("file with space" "\\" "'" "\""))
+(magit-global-args-and-files-to-s nil '("\""))
+(magit-global-args-and-files-to-s nil '("\\"))
 
 
 (provide 'magit-global)
